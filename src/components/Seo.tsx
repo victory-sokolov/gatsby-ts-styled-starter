14c6245a23cdfb6b2117c4644755284988cbec21
Viktor Sokolov <viktorsokolov.and@gmail.com>
2021-04-04 21:37:26 +0300

    init starter

---
+++ src/components/Seo.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+export const Seo = () => {
+    return <Helmet></Helmet>
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const IndexPage = () => {
+	return <h1>Hello world!</h1>;
+};
+
+export default IndexPage;
